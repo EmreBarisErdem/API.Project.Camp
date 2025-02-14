48edcbd3306379435751ced4e08bf8cae7706648
Emre Baris Erdem <erdem.emre.baris@gmail.com>
2025-02-14 12:27:43 +0300

    6-7-8. Dersler

---
+++ API.Project.Camp.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,66 @@
+﻿using API.Project.Camp.WebApi.Context;
+using API.Project.Camp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace API.Project.Camp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+		public CategoriesController(ApiContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateCategoryAsync(Category category)
+		{
+			//After Validations
+			_context.Categories.Add(category);
+			await _context.SaveChangesAsync();
+			return Ok("Kategori eklendi!");
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> CategoryList()
+		{
+			var values = await _context.Categories.ToListAsync();
+			return Ok(values);
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> DeleteCategoryAsync(int id)
+		{
+			Category? value = await _context.Categories.FindAsync(id);
+
+			_context.Categories.Remove(value);
+			await _context.SaveChangesAsync();
+			return Ok("Kategoris silme işlemi başarılı!");
+		}
+
+		[HttpGet("GetCategory")]
+		public async Task<IActionResult> GetCategory(int id)
+		{
+			Category? value = await _context.Categories.FindAsync(id);
+
+			return Ok(value);
+
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> UpdateCategoryAsync(Category category)
+		{
+			_context.Categories.Update(category);
+			await _context.SaveChangesAsync();
+			return Ok("Kategori güncelleme başarılı!");
+
+		}
+
+	}
+}
---
+++ API.Project.Camp.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Project.Camp.WebApi.Context;
+using API.Project.Camp.WebApi.Entities;
+
+namespace API.Project.Camp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Chefs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Chef>>> GetChefs()
+        {
+          if (_context.Chefs == null)
+          {
+              return NotFound();
+          }
+            return await _context.Chefs.ToListAsync();
+        }
+
+        // GET: api/Chefs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Chef>> GetChef(int id)
+        {
+          if (_context.Chefs == null)
+          {
+              return NotFound();
+          }
+            var chef = await _context.Chefs.FindAsync(id);
+
+            if (chef == null)
+            {
+                return NotFound();
+            }
+
+            return chef;
+        }
+
+        // PUT: api/Chefs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutChef(int id, Chef chef)
+        {
+            if (id != chef.ChefId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(chef).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ChefExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Chefs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Chef>> PostChef(Chef chef)
+        {
+          if (_context.Chefs == null)
+          {
+              return Problem("Entity set 'ApiContext.Chefs'  is null.");
+          }
+            _context.Chefs.Add(chef);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetChef", new { id = chef.ChefId }, chef);
+        }
+
+        // DELETE: api/Chefs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteChef(int id)
+        {
+            if (_context.Chefs == null)
+            {
+                return NotFound();
+            }
+            var chef = await _context.Chefs.FindAsync(id);
+            if (chef == null)
+            {
+                return NotFound();
+            }
+
+            _context.Chefs.Remove(chef);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ChefExists(int id)
+        {
+            return (_context.Chefs?.Any(e => e.ChefId == id)).GetValueOrDefault();
+        }
+    }
+}
